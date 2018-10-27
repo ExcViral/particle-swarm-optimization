14d2ef6e2e5c82aa95059cec5bac84b298c081a3
Viral Patel <exc.viral@gmail.com>
2018-10-28 00:23:48 +0530

    Fixed logical errors

--- python-implementation/swarm.py
+++ python-implementation/swarm.py
@@ -2,6 +2,7 @@ from numpy import *
 import matplotlib.pyplot as plt
 import scipy.linalg
 import sys
+import math
 
 from particle import *
 
@@ -41,40 +42,36 @@ class Swarm:
             self.swarm.append(Particle(w, c1, c2, dimension, bounds))
 
         # initialize gbest
-        self.gbest = self.updateGbest(self.swarm, self.mode)
+        self.gbest = self.updateGbest(self.swarm)
+        print(self.gbest)
 
     def optimize(self):
         """
         This function will start the optimization process of PSO.
 
         -----Steps-----
-        Step 1 : For each particle’s position (p) evaluate fitness, and update its pbest
-        Step 2 : Set best of pBests as gBest
-        Step 3 : Update particles' velocity
-        Step 4 : Update particles' position
+        Step 1 : evaluate gbest of whole population
+        Step 2 : Update particles' velocity
+        Step 3 : Update particles' position
+        Step 4 : Update pbest of each particle
         Step 5 : Check for convergence, stop and return gbest, if convergence is achieved, else GOTO: Step 1
 
         :return: gbest - the solution of the optimization process
         """
         for i in range(self.maxiterations):
-            print("Iteration number: ", i)
-            # Step 1
-            for j in range(len(self.swarm)):
-                self.swarm[j].updatePbest(self.mode)
-            # Step 2
-            self.updateGbest(self.swarm, self.mode)
-            # Step 3
+            print("Iteration number: ",i)
+            self.updateGbest(self.swarm)
+            self.allGbests.append(self.gbest)
+
             for j in range(len(self.swarm)):
                 self.swarm[j].updateVelocity(self.gbest)
-            # Step 4
-            for j in range(len(self.swarm)):
                 self.swarm[j].updatePosition()
-            # Step 5
-            if self.checknstop():
-                break
+                self.swarm[j].updatePbest(self.mode)
+            # if self.checknstop():
+            #     break
         return self.gbest
 
-    def updateGbest(self, population, mode):
+    def updateGbest(self, population):
         """
         This function finds the global best position any particle from the population has achieved.
 
@@ -82,29 +79,13 @@ class Swarm:
         :param mode: mode indicates whether the problem is to be minimized or maximized
         :return: gbest
         """
+        self.bestParticle = population[0]
 
-        # initially assign the first particle's position as global best position
-        gbest = population[0].position
-        gbestFitness = self.fitness(gbest)
-        # now look through all the remaining members, check their fitnesses, and update gbest accordingly
         for i in range(1, len(population)):
-            currentPosition = population[i].position
-            currentPosFitness = self.fitness(currentPosition)
-            if mode == "min":
-                if currentPosFitness < gbestFitness:
-                    gbest = currentPosition
-                    gbestFitness = currentPosFitness
-                    break
-            elif mode == "max":
-                if currentPosFitness > gbestFitness:
-                    gbest = currentPosition
-                    gbestFitness = currentPosFitness
-                    break
-            else:
-                raise Exception(mode, "is not a valid parameter, accepted parameters: 'min' or 'max'")
+            if population[i].pbestFitness > self.bestParticle.pbestFitness:
+                self.bestParticle = population[i]
 
-        self.allGbests.append(gbest)  # append gbest to allGbests list
-        return gbest
+        self.gbest = self.bestParticle.pbest
 
     def checknstop(self):
         """
@@ -117,22 +98,3 @@ class Swarm:
         if len(self.allGbests) > 30:
             self.allGbests.pop(0)
         return self.allGbests[1:] == self.allGbests[:-1]
-
-
-    def fitness(self, position):
-        """
-        Function to check the fitness of the particle. The fitness of the particle is the indication of how near the
-        position of the particle is to that of the solution.
-
-        :param position: position (solution) whose fitness is to be evaluated
-        :return: fitness value of the particle
-        """
-
-        # TODO: Implement your fitness function here
-
-        fitness = 0
-
-        return fitness
-
-
-a = Swarm(10, 2, 500, [-100,100], 1, 0.1, 0.2, "max")
